4de30e892debf527be1b98bf24a94bbaf870891c
Dmytro Slobodian <Slobodiyan@gmail.com>
2024-05-28 00:33:27 +0300

    HomeWork 5.1 => InitCommit

---
+++ Task4_1/Task5_1/src/RevenueTax.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RevenueTax {
+    public static void main(String[] args) {
+        double taxPayment;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть свій річний дохід");
+        double income = scanner.nextDouble();
+        if (income < 0){
+            System.out.println("Дохід не може бути від’ємним. Введіть коректне значення.");
+        }else if (income >= 0 && income <= 10000){
+            taxPayment = income * 0.025;
+            System.out.printf("Податок з доходу становить: %.2f.", taxPayment);
+        }else if (income > 10000 && income <= 25000){
+            taxPayment = income * 0.043;
+            System.out.printf("Податок з доходу становить: %.2f.", taxPayment);
+        }else {
+            taxPayment = income * 0.067;
+            System.out.printf("Податок з доходу становить: %.2f.", taxPayment);
+        }
+    }
+}
