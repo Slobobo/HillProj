3f930745a88f61c01a217092d60754c422119a75
Dmytro Slobodian <Slobodiyan@gmail.com>
2024-05-27 23:12:29 +0300

    Formating

--- Task4_1/src/TopShop.java
+++ Task4_1/src/TopShop.java
@@ -4,22 +4,22 @@ public class TopShop {
         byte productNumber = 1;
         float totalSales = 12153.41F;
         byte totalDays = 5;
-        float salePerDay = totalSales/totalDays;
-        String productName = "smarphone,";
+        float salePerDay = totalSales / totalDays;
+        String productName = "smarphone";
 
-        System.out.println("Product No " + productNumber + ": " + productName);
+        System.out.println("Product No " + productNumber + ": " + productName + ",");
         System.out.println("total sales for " + totalDays + " days is EUR " + totalSales + ",");
-        System.out.println("sales by day is EUR " + String.format("%.2f", salePerDay) + ".");
+        System.out.printf("sales by day is EUR %.2f.\n", salePerDay);
 
 
         productNumber = 2;
         totalSales = 10486.85F;
         totalDays = 7;
-        salePerDay = totalSales/totalDays;
-        productName = "laptop,";
+        salePerDay = totalSales / totalDays;
+        productName = "laptop";
 
-        System.out.println("Product No " + productNumber + ": " + productName);
+        System.out.println("Product No " + productNumber + ": " + productName + ",");
         System.out.println("total sales for " + totalDays + " days is EUR " + totalSales + ",");
-        System.out.println("sales by day is EUR " + String.format("%.2f", salePerDay) + ".");
+        System.out.printf("sales by day is EUR %.2f.", salePerDay);
     }
 }
