40e4b187a0a7fcac4c079a689373bd834dae8374
Dmytro Slobodian <Slobodiyan@gmail.com>
2024-06-04 17:34:51 +0300

    HomeWork 6.1 => InitCommit

---
+++ Task6_1/src/NumSum.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NumSum {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть число для знаходження суми чисел в послідовності");
+        int limit = scanner.nextInt();
+
+        int sumFor = 0;
+        for (int i = 1; i <= limit; i++) {
+            sumFor += i;
+            System.out.println("Num is " + i + " sum is " + sumFor);
+        }
+
+        int a = 1;
+        int sumDo = 0;
+        do {
+            sumDo += a;
+            System.out.println("Num is " + a + " sum is " + sumDo);
+            a++;
+        } while (a <= limit);
+
+        int b = 1;
+        int sumWhile = 0;
+        while (b <= limit) {
+            sumWhile += b;
+            System.out.println("Num is " + b + " sum is " + sumWhile);
+            b++;
+        }
+    }
+
+}
