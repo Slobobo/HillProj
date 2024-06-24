92b6a51b4edd1b00f001ca7b7b035e7675bee011
Dmytro Slobodian <Slobodiyan@gmail.com>
2024-06-24 14:18:34 +0300

    Task10 - void => return update

--- src/com/gmail/slobodiyan/task10/VariousMethods.java
+++ src/com/gmail/slobodiyan/task10/VariousMethods.java
@@ -1,50 +1,63 @@
 package com.gmail.slobodiyan.task10;
 
-import java.util.Scanner;
-
 public class VariousMethods {
     public static void main(String[] args) {
 
-        square(5);
-        volumeCylinder(3.5, 10.0);
+        int number = 5;
+        square(number);
+
+        double radius = 3.5;
+        double height = 10.0;
+        volumeCylinder(radius, height);
 
         int[] array = {10, 20, 30, 40, 50};
-        System.out.print("3. Array: [");
+        System.out.print("3.Array: [");
         print(array);
         sumArray(array);
 
-        reverseString("Hello, World!");
-        exponentiation(2, 3);
-        printMessage(4, "I love Java!");
+        String initialLine = "Hello, World!";
+        reverseString(initialLine);
+
+        int base = 2;
+        int exponent = 3;
+        exponentiation(base, exponent);
+
+        int numberRetry = 4;
+        String line = "I love Java!";
+        printMessage(numberRetry, line);
 
     }
 
-    public static void square(int number) {
-        System.out.println("1. Please enter an integer: " + number);
-        int square = number * number;
-
-        System.out.println("Square of " + number + " equals " + square + ".");
+    public static int square(int number) {
+        System.out.println("1.Please enter an integer: " + number);
+        int result = number * number;
+        System.out.println("Square of " + number + " equals " + result + ".");
         System.out.println();
+        return result;
     }
 
-    public static void volumeCylinder(double radius, double height) {
+    public static double volumeCylinder(double radius, double height) {
         if (radius <= 0 || height <= 0) {
             System.out.println("Error!!! Radius and height should be above 0.");
+            System.out.println();
+            return -1;
         } else {
             double volume = Math.PI * radius * radius * height;
 
-            System.out.println("2. Volume of cylinder with radius " + radius + " and high " + height + " equals " + volume + ".");
+            System.out.println("2.Volume of cylinder with radius " + radius + " and high " + height + " equals " + volume + ".");
             System.out.println();
+            return volume;
         }
     }
 
-    public static void sumArray(int[] array) {
+    public static int sumArray(int[] array) {
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
         System.out.println("Total sum of array equals " + sum + ".");
         System.out.println();
+        return sum;
     }
 
     public static void print(int[] array) {
@@ -55,10 +68,9 @@ public class VariousMethods {
             }
         }
         System.out.println("]");
-        //System.out.println();
     }
 
-    public static void reverseString(String str) {
+    public static char[] reverseString(String str) {
         if (!str.isEmpty()) {
             System.out.print("4. Please enter line: ");
             System.out.println(str);
@@ -69,12 +81,14 @@ public class VariousMethods {
             }
             System.out.println();
             System.out.println();
+            return charArray;
         } else {
             System.out.println("ERROR!!! Please enter valid line!!!");
+            return "".toCharArray();
         }
     }
 
-    public static void exponentiation(int base, int exponent) {
+    public static int exponentiation(int base, int exponent) {
 
         System.out.println("5. Please enter a: " + base);
         System.out.println("Please enter b: " + exponent);
@@ -83,6 +97,7 @@ public class VariousMethods {
         int finalResult = (int) Math.round(initialResult);
         System.out.println("Result " + base + "^" + exponent + " equals " + finalResult + ".");
         System.out.println();
+        return finalResult;
     }
 
     public static void printMessage(int numberRetry, String text) {
